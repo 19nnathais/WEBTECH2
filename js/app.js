31a4421071a2bb2e803a2badb3ef08894124d772
Tuan Hung <hoangtuanhung220901@gmail.com>
2023-03-04 07:29:42 +0700

    up

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const menuIcon = document.querySelector('.menu-icon');
+const menuLinks = document.querySelector('ul');
+
+menuIcon.addEventListener('click', () => {
+  menuLinks.classList.toggle('active');
+});
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
