76bbac44e2429793747f5a797d04fc2f3714c34e
Tuan Hung <hoangtuanhung220901@gmail.com>
2023-03-05 20:48:22 +0700

    up

--- js/app.js
+++ js/app.js
@@ -54,3 +54,22 @@ var swiper1 = new Swiper(".mySwiper", {
   });
   swiper1.slideNext();
   swiper2.slideNext();
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" actives", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " actives";
+  setTimeout(showSlides, 3000); 
+}
